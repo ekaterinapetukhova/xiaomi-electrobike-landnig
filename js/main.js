9ca1aa0ec5e8d30fd4a20a92078a4d2795a403b3
ekaterinapetukhova <katepthv12@gmail.com>
2022-08-05 22:42:15 +0300

    header, bike section html, css, js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        vertical: true,
+        mobileFirst: true,
+        autoplay: true
+    });
+
+});
