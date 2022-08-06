7923d10e39715fd15098f987d7cf50a71beca30f
ekaterinapetukhova <katepthv12@gmail.com>
2022-08-07 00:42:35 +0300

    create full landing

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-$(function() {
-    $('.bike-slider').slick({
-        arrows: false,
-        dots: true,
-        vertical: true,
-        mobileFirst: true,
-        autoplay: true
-    });
+$(function () {
+  $(".bike-slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+  });
 
+  $(".slider__items").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+  });
+});
+
+$('.bike__btn').on( 'click', function(){ 
+  var el = $(this);
+  var dest = el.attr('href');
+  if(dest !== undefined && dest !== '') {
+      $('html').animate({ 
+        scrollTop: $(dest).offset().top 
+      }, 1000 
+      );
+  }
+  return false;
 });
